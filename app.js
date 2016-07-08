6ab50f1c9447f4848989732587d14e8933745e39
kendallgoodwin <kendall@kendallgoodwin.com>
2016-07-08 11:02:17 -0700

    navbar collapse functions on mobile view

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
-var app = angular.module('CakeApp', ['ngAnimate', 'ui.bootstrap.collapse']);
+var app = angular.module('CakeApp', ['ngAnimate', 'ui.bootstrap.collapse', 'ui.bootstrap.dropdown']);
 
 app.controller('HomeCtrl', ['$scope', function($scope) {
 	$scope.name = 'Kendall Maniulit'
 
+	// Enables clickable collapse navbar in mobile view
 	$scope.isCollapsed = true;
+
+	// Enables dropdown menu in navbar
+	$scope.status = {
+    isopen: false
+  };
 }])
