39125ba1280ae7a13c2240261af5e8f31ad514f5
kendallgoodwin <kendall@kendallgoodwin.com>
2016-07-08 10:42:14 -0700

    dropdown in navbar working

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-var app = angular.module('CakeApp', ['ngAnimate']);
+var app = angular.module('CakeApp', ['ngAnimate', 'ui.bootstrap.collapse']);
 
 app.controller('HomeCtrl', ['$scope', function($scope) {
 	$scope.name = 'Kendall Maniulit'
+
+	$scope.isCollapsed = true;
 }])
